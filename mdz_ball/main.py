f6a1999dc349597bd3d87013bd6b702c66181db3
nanosgr <sebastian.g.rios@gmail.com>
2024-02-05 09:38:18 -0300

    Actualiza período

--- mdz_ball/main.py
+++ mdz_ball/main.py
@@ -3,7 +3,7 @@ from analisis_facturas import analisis_facturacion
 if __name__ == "__main__":
     desde = '2023-10-01'
     hasta = '2023-10-31'
-    archivo = '/home/sebastian/Documentos/win_files/Estudio/MendozaBalloons/pasajeros Dic2023.xlsx'
+    archivo = '/home/sebastian/Documentos/win_files/Estudio/MendozaBalloons/pasajeros Ene2024.xlsx'
 
     analisis_facturacion(desde, hasta, archivo)
     exit(0)
